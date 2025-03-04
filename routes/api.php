17adb6cfab37394f2f45b13f7ba62f4723ae0f6a
Thijs Kraaijenbos <47988622+ThijsKraaijenbos@users.noreply.github.com>
2025-03-04 11:59:47 +0100

    Project setup

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Api\V1\ExerciseController;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::get('/users', [ExerciseController::class, 'index']);
+});
