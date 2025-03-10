4df17e6d3ebdfc0954318e1f6784c745641564fb
Hidde-Scheringa <hescheringa@gmail.com>
2025-03-10 14:58:26 +0100

    added a letters table and filled in the data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ExercisesFiller;
+use App\Http\Controllers\LetterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,5 +11,9 @@ Route::get('/', function () {
 Route::get('/filler', [ExercisesFiller::class, 'index'])->name('filler.index');
 
 Route::post('/filler', [ExercisesFiller::class, 'store'])->name('filler.store');
+
+Route::get('/letters', [LetterController::class, 'index'])->name('letters.index');
+
+Route::post('/letters', [LetterController::class, 'store'])->name('letters.store');
 
 
