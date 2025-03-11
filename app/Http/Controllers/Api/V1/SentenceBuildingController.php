9d8cdb7e1832e5a1c03fd162c87f145ecc1300c5
alana <alana.l.f@upcmail.nl>
2025-03-11 14:16:38 +0100

    files moved to v1 map and routes changed to proper naming convention

--- app/Http/Controllers/Api/V1/SentenceBuildingController.php
+++ app/Http/Controllers/Api/V1/SentenceBuildingController.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Models\SentenceBuilding;
-use App\Models\Word;
-use Illuminate\Http\Request;
 
 class SentenceBuildingController extends Controller
 {
