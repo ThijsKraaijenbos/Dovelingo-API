8c265d7f61c7247cd2dd923889c6d869c4c9fd6f
Hidde-Scheringa <hescheringa@gmail.com>
2025-03-11 14:18:31 +0100

    spelling error fixed

--- resources/views/trophy-filler.blade.php
+++ resources/views/trophy-filler.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload trophies</title>
+    <title>Upload trophy</title>
 </head>
 <body>
 <h1>Upload van trophies en scores</h1>
