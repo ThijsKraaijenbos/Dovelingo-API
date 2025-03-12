e5a049e82ec3e8743f2f02210dea87aa0995843a
alana <alana.l.f@upcmail.nl>
2025-03-12 14:13:04 +0100

    lesson get

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Lesson extends Model
 {
-    //
+    protected $fillable = [
+        'lesson_name',
+    ];
+
+    public function word(): HasMany
+    {
+        return $this->hasMany(Word::class);
+    }
+
+    public function sentenceBuilding(): HasMany
+    {
+        return $this->hasMany(SentenceBuilding::class);
+    }
+
+    public function fillInTheBlanks(): HasMany
+    {
+        return $this->hasMany(FillInTheBlanks::class);
+    }
 }
